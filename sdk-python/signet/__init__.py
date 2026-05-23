a0764e588c6123b446da675268e95831a9bd8097
ashutosh887 <ashutoshj887@gmail.com>
2026-05-23 16:44:28 +0530

    feat: Add RootIdentity and attestation functions to the SDK module

--- sdk-python/signet/__init__.py
+++ sdk-python/signet/__init__.py
@@ -4,6 +4,7 @@ from .client import register, verify, submit, revoke, audit, get_agent, get_enve
 from .wrap import wrap, delegate
 from .hsm import Signer, SoftwareSigner, SoftwareClassicalSigner, PKCS11Signer
 from .mcp import SignetMCPMiddleware
+from .root import RootIdentity, attest_agent, verify_attestation
 from . import kem
 
 __version__ = "0.3.0"
@@ -26,5 +27,8 @@ __all__ = [
     "SoftwareClassicalSigner",
     "PKCS11Signer",
     "SignetMCPMiddleware",
+    "RootIdentity",
+    "attest_agent",
+    "verify_attestation",
     "kem",
 ]
